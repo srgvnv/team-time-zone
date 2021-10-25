a1ed35fae039095224e72e36e58b5916e87b551b
Sergey Ivanov <sergey.ivanov@drupalsquad.com>
2021-10-25 18:55:47 +0300

    JSON data with user and time zones

--- src/components/TimeZones/TimeZones.js
+++ src/components/TimeZones/TimeZones.js
@@ -1,3 +1,12 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+import data from './data.json';
+
 function TimeZones() {
   return (
     <div>time zones page</div>
